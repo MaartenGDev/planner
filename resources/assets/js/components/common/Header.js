490a7ed34d5fbdeffa2482b60ccb090d2047ccf5
joostlawerman <joost.lawerman@gmail.com>
2016-09-21 15:19:07 +0200

    Header

--- resources/assets/js/components/common/Header.js
+++ resources/assets/js/components/common/Header.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {Link, IndexLink} from 'react-router'
 
-const Header = () => {
-    return (
-        <nav>
-            <ul>
-                <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
-                <li><Link to="/events" activeClassName="active">Events</Link></li>
-                <li><Link to="/login" activeClassName="active">Login</Link></li>
-            </ul>
-        </nav>
-    )
-};
-
-export default Header;
+export default (
+    <nav>
+        <ul>
+            <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+            <li><Link to="/events" activeClassName="active">Events</Link></li>
+            <li><Link to="/login" activeClassName="active">Login</Link></li>
+        </ul>
+    </nav>
+)
