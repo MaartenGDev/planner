0f913874dc68179f76d52b91f95d6387ff49a74c
MaartenGDev <maarten.grootoonk@gmail.com>
2016-09-21 14:30:28 +0200

    Added stateless function for template

--- resources/assets/js/components/events/Event.js
+++ resources/assets/js/components/events/Event.js
@@ -1,17 +1,22 @@
 import React from "react";
 
-class Event extends React.Component {
-    render() {
-        const {title, description, start, end, id} = this.props;
-        return (
-            <tr>
-                <td>{title}</td>
-                <td>{description.substr(0,30)+ "..."}</td>
-                <td><b>{start}</b> <i>to</i> <b>{end}</b></td>
-                <td><a className="btn btn-default link-button event-action-btn" href={"/events/" + id + "/edit"}>Edit</a></td>
-                <td><span className="btn btn-danger link-button event-action-btn" href="" onClick={this.props.removeEvent}>Remove</span></td>
-            </tr>
-        )
-    }
-}
+const Event = ({title, description, start, end, id}) => {
+    return (
+        <tr>
+            <td>{title}</td>
+            <td>{description.substr(0, 30) + "..."}</td>
+            <td><b>{start}</b> <i>to</i> <b>{end}</b></td>
+            <td>
+                <a className="btn btn-default link-button event-action-btn" href={"/events/" + id + "/edit"}>
+                    Edit
+                </a>
+            </td>
+            <td>
+                <span className="btn btn-danger link-button event-action-btn" onClick={this.props.removeEvent}>
+                    Remove
+                </span>
+            </td>
+        </tr>
+    )
+};
 export default Event;
